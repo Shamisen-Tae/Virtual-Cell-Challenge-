03fd40d31147523dc33889a9b2ecce3f860edbd0
vivianwhite <vivcat980@gmail.com>
2025-11-22 18:20:34 -0800

    launching elasticnet (typed ridge by mistake earlier)

--- launch_eda.sh
+++ launch_eda.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=200G
-#SBATCH --output=slurm/cv_baseline_%j.out
-#SBATCH --error=slurm/cv_baseline_%j.err
+#SBATCH --output=slurm/cv_elasticnet_%j.out
+#SBATCH --error=slurm/cv_elasticnet_%j.err
 
 source ~/.bashrc
 conda activate vcc_py3.10
@@ -15,6 +15,6 @@ conda activate vcc_py3.10
 export NUMBA_CACHE_DIR=/scratch/st-evanesce-1/vivian/.numba_cache
 export MPLCONFIGDIR=/scratch/st-evanesce-1/vivian/.matplotlib_cache
 
-python eda_cv.py --model baseline
-python eda_cv.py --model ridge
+#python eda_cv.py --model baseline
+python eda_cv.py --model elasticnet
 
